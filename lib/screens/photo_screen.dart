c55c9ad411c1d95d22b5c9354164585df00f926f
devspo <devspo@inbox.ru>
2020-09-04 01:51:27 +0400

    StaggeredAnimaton username

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import '../widgets/widgets.dart';
 import '../res/res.dart';
 
-class FullScreenImage extends StatelessWidget {
+class FullScreenImage extends StatefulWidget {
   final String photo;
   final String altDescription;
   final String userName;
@@ -20,6 +20,31 @@ class FullScreenImage extends StatelessWidget {
     this.heroTag,
     this.userPhoto
   }) : super(key: key);
+
+  @override
+  _FullScreenImageState createState() => _FullScreenImageState();
+}
+
+class _FullScreenImageState extends State<FullScreenImage> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1500),
+      vsync: this,
+    )..forward();
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,31 +68,21 @@ class FullScreenImage extends StatelessWidget {
           Column(
             children: <Widget>[
               Hero(
-                tag: heroTag,
-                child: Photo(photo: photo)
+                tag: widget.heroTag,
+                child: Photo(photo: widget.photo)
               ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: altDescription != null ? Description(description: altDescription) : Description(description: 'Beautiful girl in a yellow dress with a flower on her head in the summer in the forest. Beautiful girl in a yellow dress with a flower on her head in the summer in the forest. Beautiful girl in a yellow dress with a flower on her head in the summer in the forest'),
+                child: widget.altDescription != null ? Description(description: widget.altDescription) : Description(description: 'Beautiful girl in a yellow dress with a flower on her head in the summer in the forest. Beautiful girl in a yellow dress with a flower on her head in the summer in the forest. Beautiful girl in a yellow dress with a flower on her head in the summer in the forest'),
               ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        UserAvatar(userPhoto),
-                        SizedBox(width: 6,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Name(name: 'Kirill Adeshchenko'),
-                            Username(userName: '@kaparray'),
-                          ],
-                        )
-                      ],
+                    StaggerAnimation(
+                      controller: _controller.view,
+                      userPhoto: widget.userPhoto,
                     ),
                   ],
                 ),
@@ -164,6 +179,73 @@ class Description extends StatelessWidget {
       maxLines: 3,
       overflow: TextOverflow.ellipsis,
       style: AppStyles.h3.copyWith(color: AppColors.manatee),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> opacityUsername;
+  final String userPhoto;
+
+  StaggerAnimation({Key key, this.controller, this.userPhoto}) :
+    opacity = Tween<double>(
+      begin: 0.0,
+      end: 1.0
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.0, 0.5,
+          curve: Curves.ease
+        ),
+      ),
+    ),
+
+    opacityUsername = Tween<double>(
+      begin: 0.0,
+      end: 1.0
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.5, 1.0,
+          curve: Curves.ease
+        ),
+      ),
+    ),
+
+    super(key: key);
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Row(
+      children: <Widget>[
+        Opacity(
+          opacity: opacity.value,
+          child: UserAvatar(userPhoto)
+        ),
+        SizedBox(width: 6,),
+        Opacity(
+          opacity: opacityUsername.value,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Name(name: 'Kirill Adeshchenko'),
+              Username(userName: '@kaparray'),
+            ],
+          ),
+        ),
+      ]
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
     );
   }
 }
